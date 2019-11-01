6e4c6b7f9d3165fe2e807b0d5029b664dafce7a9
Pavel Perestoronin <eigenein@gmail.com>
2019-11-01 16:29:08 +0100

    Chore: serialization unit tests

--- src/core/persistence/serialization.rs
+++ src/core/persistence/serialization.rs
@@ -59,3 +59,30 @@ fn deserialize_u64(blob: &[u8]) -> Result<u64> {
 fn deserialize_f64(blob: &[u8]) -> Result<f64> {
     Ok(f64::from_bits(deserialize_u64(blob)?))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! value_ok {
+        ($name:ident, $value:expr) => {
+            #[test]
+            fn $name() -> crate::Result<()> {
+                let value = $value;
+                let (type_, blob) = value.serialize();
+                assert_eq!(Value::deserialize(type_, blob)?, value);
+                Ok(())
+            }
+        };
+    }
+
+    value_ok!(none_ok, Value::None);
+    value_ok!(false_ok, Value::Boolean(false));
+    value_ok!(true_ok, Value::Boolean(true));
+    value_ok!(image_url_ok, Value::ImageUrl("https://google.com".into()));
+    value_ok!(text_ok, Value::Text("Hello, world!".into()));
+    value_ok!(bft_ok, Value::Bft(3));
+    value_ok!(celsius_ok, Value::Celsius(20.0));
+    value_ok!(counter_ok, Value::Counter(42));
+    // TODO: the rest.
+}
